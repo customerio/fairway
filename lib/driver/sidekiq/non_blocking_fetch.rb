5202a1e81dd7b9d8be604a67c2705c1d51b0f999
John Allison <jrallison@gmail.com>
2013-02-11 02:54:45 -0500

    Move non blocking fetch to a lua script

--- lib/driver/sidekiq/non_blocking_fetch.rb
+++ lib/driver/sidekiq/non_blocking_fetch.rb
@@ -12,10 +12,28 @@ module Driver
       end
 
       def retrieve_work
-        ::Sidekiq.logger.debug "#{self.class.name}#retrieve_work"
+        ::Sidekiq.logger.debug "#{self.class.name}#retrieve_work #{queues_cmd}"
 
-        if (work = super)
+        work = ::Sidekiq.redis do |conn|
+          script = <<-SCRIPT
+            -- take advantage of non-blocking scripts
+            for i = 1, #KEYS do
+              local work = redis.call('rpop', KEYS[i]);
+
+              if work then
+                return {KEYS[i], work};
+              end
+            end
+
+            return nil;
+          SCRIPT
+
+          conn.eval(script, queues_cmd.map{|q| "#{namespace}#{q}"})
+        end
+
+        if (work)
           ::Sidekiq.logger.debug "#{self.class.name}#retrieve_work got work"
+          work = UnitOfWork.new(*work)
         else
           ::Sidekiq.logger.debug "#{self.class.name}#retrieve_work got nil"
         end
@@ -26,6 +44,25 @@ module Driver
       def queues_cmd
         queues = @queues.shuffle.uniq
         queues << 0 # return immediately if nothing on queue
+      end
+
+      def namespace
+        @namespace ||= begin
+          namespaces = []
+
+          ::Sidekiq.redis do |conn|
+            wrapper = conn
+
+            while defined?(wrapper.redis)
+              namespaces.unshift(wrapper.namespace)
+              wrapper = wrapper.redis
+            end
+
+            namespace = namespaces.join(":")
+            namespace += ":" unless namespace.blank?
+            namespace
+          end
+        end
       end
     end
   end
