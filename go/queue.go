dd7d4289a80730629a3b1c23aca1d33f1e755cbe
John Allison <jrallison@gmail.com>
2014-03-05 11:58:37 +0100

    Pull should accept an integer for controling resending.

--- go/queue.go
+++ go/queue.go
@@ -9,8 +9,8 @@ func NewQueue(conn Connection, name string) *Queue {
 	return &Queue{conn, name}
 }
 
-func (q *Queue) Pull(timestamp int64) (string, *Msg) {
-	return q.conn.Configuration().scripts().pull(q.name, int(timestamp))
+func (q *Queue) Pull(resendTimeframe int) (string, *Msg) {
+	return q.conn.Configuration().scripts().pull(q.name, resendTimeframe)
 }
 
 func (q *Queue) Inflight() []string {
